e00f923e7b3c83eae76d7a7d920cd2cb5fa33332
arthur chiquet <arthur.chiquet@gmail.com>
2023-06-14 16:53:32 +0200

    options

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: Faker::Address.full_address,
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: Faker::Company.name,
+  address: Faker::Address.full_address,
+  description: Faker::Lorem.paragraph,
+  price_per_night: 100,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: Faker::Company.name,
+  address: Faker::Address.full_address,
+  description: Faker::Lorem.paragraph,
+  price_per_night: 68,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: Faker::Company.name,
+  address: Faker::Address.full_address,
+  description: Faker::Lorem.paragraph,
+  price_per_night: 125,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: Faker::Company.name,
+  address: Faker::Address.full_address,
+  description: Faker::Lorem.paragraph,
+  price_per_night: 110,
+  number_of_guests: 2
+)
